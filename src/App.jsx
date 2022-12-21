a1cfce584d87ada5363d818f075a89c2a885321f
frankchiu12 <si-un_chiu@brown.edu>
2022-12-21 13:29:55 -0500

    idk

--- src/App.jsx
+++ src/App.jsx
@@ -4,9 +4,14 @@ import './App.css'
 
 function App() {
   return (
-    <button className='w-36 h-18 bg-[#528B3D] p-2 rounded-full hover:bg-green-800 hover:text-black'>
-    Join Us
-    </button>
+    <div class="bg-hero">
+      <h1 id="" className="font-sans text-7xl text-center regular">
+        Chess@Brown
+      </h1>
+      <button className="w-36 h-18 bg-[#528B3D] p-2 rounded-full hover:bg-green-800 hover:text-black">
+      Join Us
+      </button>
+    </div>
   )
 }
 
