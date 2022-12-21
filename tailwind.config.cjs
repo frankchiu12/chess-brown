ca4105397721e03464a39afc4c9bf85697223bca
frankchiu12 <si-un_chiu@brown.edu>
2022-12-21 13:08:44 -0500

    hi

--- tailwind.config.cjs
+++ tailwind.config.cjs
@@ -1,8 +1,15 @@
 /** @type {import('tailwindcss').Config} */
 module.exports = {
-  content: [],
+  content: [
+    "./index.html",
+    "./src/**/*.{js,ts,jsx,tsx}",
+  ],
   theme: {
-    extend: {},
+    extend: {
+      backgroundImage: {
+        'hero': "url('src/assets/chess_fabric_background.jpeg')",
+      }
+    },
   },
   plugins: [],
 }
